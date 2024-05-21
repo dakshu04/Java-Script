0abf87babd075d701b399dbb1f6ae75fe4f300da
dakshu04 <dakshgrows@gmail.com>
2024-05-22 05:06:45 +0530

    Done dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+console.log([3+3,"Daksh"]);
+console.log(typeof("daksh"));
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object

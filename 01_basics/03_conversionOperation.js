886b368da3698cc56143dac51805f0fd25f58eb2
dakshu04 <dakshgrows@gmail.com>
2024-05-22 05:30:19 +0530

    Done witn comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "33";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,3 +12,14 @@ console.log(typeof valueInNumber);
 // 1 => true; 0 => false
 // "" => false
 // "daksh" => true
+
+// *********** Operations ************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(2**3); // 2^3
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
